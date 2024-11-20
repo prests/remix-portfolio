da6a616a64f074950bc8ecd3a4bb090315b7f3e1
Shayne Preston <shayne.acc.records+tech@gmail.com>
2024-11-19 23:12:42 -0400

    feat(metadata): setup index route (#4)
    Co-authored-by: Shayne Preston <shayne.preston@protonmail.com>

--- playwright/tests/example.spec.ts
+++ playwright/tests/example.spec.ts
@@ -5,6 +5,5 @@ import { test } from 'playwright/playwright-base.fixture';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Remix Hono Template/);
+  await expect(page).toHaveTitle(/Shayne Preston/);
 });
