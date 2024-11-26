e2d7ae53fa61ffd11943d4ccebe15c232c2b5ea6
Shayne Preston <shayne.acc.records+tech@gmail.com>
2024-11-26 19:35:59 -0400

    feat(logo): initial setup (#10)
    Co-authored-by: Shayne Preston <shayne.preston@protonmail.com>

---
+++ src/remix-app/assets/Logo/logo.stylex.ts
@@ -0,0 +1,7 @@
+import * as stylex from '@stylexjs/stylex';
+
+import { tokens } from '../../themes/tokens.stylex';
+
+export const logoVars = stylex.defineVars({
+  lineColor: tokens.color_text_brand,
+});
